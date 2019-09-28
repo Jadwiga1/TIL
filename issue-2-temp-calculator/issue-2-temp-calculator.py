970e0d9b82f27907c5e36a3346d2df77c1930841
Jadwiga1 <54481147+Jadwiga1@users.noreply.github.com>
2019-09-28 20:39:21 +0200

    My first git push :)

---
+++ issue-2-temp-calculator/issue-2-temp-calculator.py
@@ -0,0 +1,15 @@
+print("KALKULATOR TEMPERATUR")
+print("Wybierz rodzaj konwersji:")
+print("1 - stopnie Celsjusza na Farenheita")
+print("2 - stopnie Farenheita na Celsjusza")
+
+type_conversion = input("Wybrany rodzaj konwersji (oznaczenie liczbowe): ")
+temperature_value = float(input("Wpisz wartość: "))
+
+if type_conversion == "1":
+    degrees_farenheit = temperature_value * 1.8 + 32
+    print("Odpowiedź: {}°C to {}°F".format(temperature_value, degrees_farenheit))
+
+if type_conversion == "2":
+    degrees_celsius = (temperature_value - 32) / 1.8
+    print("Odpowiedź: {}°F to {}°C".format(temperature_value, degrees_celsius))
