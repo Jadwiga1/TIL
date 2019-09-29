104e3a64fe1c278de8be250ba0dd4733a355f548
Jadwiga1 <54481147+Jadwiga1@users.noreply.github.com>
2019-09-29 11:28:38 +0200

    Changed input(text) & if>elif

--- issue-2-temp-calculator/issue-2-temp-calculator.py
+++ issue-2-temp-calculator/issue-2-temp-calculator.py
@@ -3,10 +3,10 @@ print("Wybierz rodzaj konwersji:")
 print("1 - stopnie Celsjusza na Farenheita")
 print("2 - stopnie Farenheita na Celsjusza")
 
-type_conversion = input("Wybrany rodzaj konwersji (oznaczenie liczbowe): ")
+type_conversion = input("Wpisz interesujący cię rodzaj konwersji (oznaczenie liczbowe): ")
 
 while type_conversion != "1" or type_conversion != "2":
-    type_conversion = input("Podana konwersja nieistnieje. Wybierz konwersję z listy: ")
+    type_conversion = input("Podana konwersja nie istnieje. Wybierz konwersję z listy: ")
     if type_conversion == "1" or type_conversion == "2":
         break
 
@@ -16,6 +16,6 @@ if type_conversion == "1":
     degrees_farenheit = temperature_value * 1.8 + 32
     print("Odpowiedź: {}°C to {}°F".format(temperature_value, degrees_farenheit))
 
-if type_conversion == "2":
+elif type_conversion == "2":
     degrees_celsius = (temperature_value - 32) / 1.8
     print("Odpowiedź: {}°F to {}°C".format(temperature_value, degrees_celsius))
